949ffaaa2480f2315872c1d604a72198dda96e3b
AnnaTSW0609 <38995185+AnnaTSW0609@users.noreply.github.com>
2019-02-22 10:34:07 +0800

    Update Assignment_1.R

--- Assignment_1.R
+++ Assignment_1.R
@@ -16,18 +16,16 @@ miR_treatment <- function(gsm) {
 sapply(GSMList(gse),miR_treatment) # this steps returns data like a loop from function 
 
 pd <- data.frame(miR_treatment=as.factor(sapply(GSMList(gse),miR_treatment))) # this step saves what we are interested into a dataframe
-pd
 
 pd$miR_treatment <- as.factor(pd$miR_treatment) # simplification of the info in column
 levels(pd$miR_treatment) <- c("control","miR203") # this changes the entry into a readable format
-pd 
+
 # this matches the phenodata as we read the raw data 
 celfiles <- paste0('data/',rownames(pd),'.CEL')
 affydata <- read.affybatch(celfiles,phenoData = new("AnnotatedDataFrame",pd))
 phenoData(affydata)
 
 eset <- rma(affydata) # performing RMA preprocessing with our raw data
-eset
 plotDensity(exprs(eset),xlab='log intensity',main="feature level densities after RMA",lwd=2) # plotting the data
 
 pData(eset) # viewing the phenodata
@@ -35,11 +33,9 @@ pData(eset) # viewing the phenodata
 library(limma) # loading the limma library for batch analysis 
 model <- model.matrix( ~ 0 + eset$miR_treatment) # linear model dependent on 0 [deduce the intercept]; model matrix to specify the design
 colnames(model) <- levels(eset$miR_treatment) 
-model # represent the two possible values of the miR treatements
 
 # look at where the miR treatments differ
 contrasts <- makeContrasts(miR203 - control, levels=model) # contrast matrix for stat testing (difference between control/miR203 data)
-contrasts
 
 # fitting our data to the models we have built  
 fit <- lmFit(eset, model) 
@@ -50,7 +46,6 @@ fitted.ebayes <- eBayes(fitted.contrast) # prior distribution is derived from da
 
 # creating a character matrix of the top 10 probe sets
 ps <- rownames(topTable(fitted.ebayes))  
-ps
 
 ## Mapping probe names to gene names and other identifiers
 # download the hug133plus2 annotation set if needed
@@ -78,7 +73,11 @@ downregulated <- AnnotationDbi::select(hgu133plus2.db,ps2_down,c("SYMBOL","ENTRE
 write.csv(upregulated, "Upregulated_Genes_LFC1.csv") 
 write.csv(downregulated,'Downregulated_Genes_LFC1.csv')
 
-# getting the up/down-regulated genes with 2 times LFC for further narrowing down
+# Generating the volcano plot with genes with more than 2-times LFC circled in red
+volcanoplot(fitted.ebayes, main=sprintf("%d features pass our cutoffs",nrow(interesting_genes)))
+points(interesting_genes[['logFC']],-log10(interesting_genes[['P.Value']]),col='red')
+
+# getting the up/down-regulated genes with 2 times LFC for further narrowing down, those circled in red in the step above
 interesting_genes <- topTable(fitted.ebayes,number = Inf,p.value = 0.05,lfc=2)
 interesting_genes_up <- rownames(interesting_genes[interesting_genes$logFC > 0,])
 interesting_genes_down <- rownames(interesting_genes[interesting_genes$logFC < 0,])
